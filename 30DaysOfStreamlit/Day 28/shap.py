cc0e6053ccc13dad6aa9bc12753ea363af0ee13c
whoisorioki <adrianorioki@gmail.com>
2023-03-30 23:43:05 +0300

    Week Updates!

--- 30DaysOfStreamlit/Day 28/shap.py
+++ 30DaysOfStreamlit/Day 28/shap.py
@@ -25,4 +25,46 @@ def load_model(X, y):
         "eval_metric": "logloss",
         "n_jobs": -1,
     }
-    model = 
+    model = xgboost.train(params, d_train, 10, evals = [(d_test, "test")], verbose_eval=100, early_stopping_rounds=20)
+    return model
+
+st.title("`streamlit-shap` for displaying SHAP plots in a Streamlit app")
+
+with st.expander('About the app'):
+    st.markdown('''[`streamlit-shap`](https://github.com/snehankekre/streamlit-shap) is a Streamlit component that provides a wrapper to display [SHAP](https://github.com/slundberg/shap) plots in [Streamlit](https://streamlit.io/). 
+                    The library is developed by our in-house staff [Snehan Kekre](https://github.com/snehankekre) who also maintains the [Streamlit Documentation](https://docs.streamlit.io/) website.
+                ''')
+
+st.header('Input data')
+X,y = load_data()
+X_display,y_display = shap.datasets.adult(display=True)
+
+with st.expander('About the data'):
+    st.write('Adult census data is used as the example dataset.')
+with st.expander('X'):
+    st.dataframe(X)
+with st.expander('y'):
+    st.dataframe(y)
+
+st.header('SHAP output')
+
+# train XGBoost model
+model = load_model(X, y)
+
+# compute SHAP values
+explainer = shap.Explainer(model, X)
+shap_values = explainer(X)
+
+with st.expander('Waterfall plot'):
+    st_shap(shap.plots.waterfall(shap_values[0]), height=300)
+with st.expander('Beeswarm plot'):
+    st_shap(shap.plots.beeswarm(shap_values), height=300)
+
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+with st.expander('Force plot'):
+    st.subheader('First data instance')
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
+    st.subheader('First thousand data instance')
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
